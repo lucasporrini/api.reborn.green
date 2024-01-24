e8db7d7bc9cc2b4a411f0785846b262b62c78ad2
lucasporrini <2608lucas@gmail.com>
2024-01-24 14:32:14 +0100

    Test: testing

--- app/controllers/ApiController.php
+++ app/controllers/ApiController.php
@@ -24,7 +24,7 @@ class ApiController
             return;
         }
 
-        $hash = hash_hmac('sha1', $payload, $secret); // On génère le hash
+        $hash = hash_hmac('sha1', $payload, $secret); // On génère le hash (test)
         
         if (hash_equals('sha1=' . $hash, $githubSignature)) { 
             // On integre un message de validation avec la date et l'heure
