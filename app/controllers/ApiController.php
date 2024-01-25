0058be296cef5377d01e57a8cfadcc97c1608057
lucasporrini <2608lucas@gmail.com>
2024-01-25 14:39:05 +0100

    Debug: testing return

--- app/controllers/ApiController.php
+++ app/controllers/ApiController.php
@@ -252,6 +252,7 @@ class ApiController
 
     public function get_company()
     {
+        print_r(apache_request_headers());
         // On récupère le token dans le header
         $headers = apache_request_headers();
         $token = $headers['Authorization'];
