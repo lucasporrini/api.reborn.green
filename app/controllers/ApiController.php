a42418cc76b854845e5ffbdc5193cd61064e12a2
lucasporrini <2608lucas@gmail.com>
2024-01-25 16:16:41 +0100

    Debug: test

--- app/controllers/ApiController.php
+++ app/controllers/ApiController.php
@@ -195,11 +195,10 @@ class ApiController
                     $products = $this->apiModel->get_products();
                 }
             }
-
+            dd($products);
             // Retourner les données en json
             header('Content-Type: application/json');
-            $test = json_encode($products, JSON_UNESCAPED_UNICODE);
-            dd($test);
+            echo json_encode($products, JSON_UNESCAPED_UNICODE);
         }
     }
 
