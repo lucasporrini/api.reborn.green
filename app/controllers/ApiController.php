4f0d6b6f9fdb09dcb9b1dbf91f7d4254f7357f19
lucasporrini <2608lucas@gmail.com>
2024-02-08 11:40:41 +0100

    Debug: Debuging API

--- app/controllers/ApiController.php
+++ app/controllers/ApiController.php
@@ -467,8 +467,9 @@ class ApiController
         if($this->apiModel->middleware_auth($token)) {
             // Récupérer les données
             $photos = json_decode(file_get_contents('php://input'), true);
+            print_r(gettype($photos));
+            print_r($photos);
             $editedProduct = $this->apiModel->edit_product_photo($slug, json_encode($photos));
-            echo $editedProduct;
             
             if($editedProduct !== null) {
                 header('Content-Type: application/json');
