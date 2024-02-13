eb05d0116a1c2a469a446e3167ac5bfdf1c4213c
lucasporrini <2608lucas@gmail.com>
2024-02-13 16:07:25 +0100

    Edited: edit product function

--- app/controllers/ApiController.php
+++ app/controllers/ApiController.php
@@ -466,10 +466,10 @@ class ApiController
         
         if($this->apiModel->middleware_auth($token)) {
             // Récupérer les données
-            $photos = json_decode(file_get_contents('php://input'), true);
-
+            $data = json_decode(file_get_contents('php://input'), true);
+            print_r($data);
             // On fait la modification en base de données
-            $editedProduct = $this->apiModel->edit_product_photo($slug, $photos['photos']);
+            $editedProduct = $this->apiModel->edit_product_photo($slug, $data);
 
             if($editedProduct !== null) {
                 header('Content-Type: application/json');
