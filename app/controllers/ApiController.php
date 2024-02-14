da81f271412e362caf88ac86d0549e9ca07bae68
lucasporrini <2608lucas@gmail.com>
2024-02-14 16:48:14 +0100

    Edited: edit product function

--- app/controllers/ApiController.php
+++ app/controllers/ApiController.php
@@ -467,6 +467,8 @@ class ApiController
         if($this->apiModel->middleware_auth($token)) {
             // Récupérer les données
             $data = json_decode(file_get_contents('php://input'), true);
+
+            $data['storage_location'] == 'dépôt' ? $data['location_id'] = null : '';
             
             // On fait la modification en base de données
             $editedProduct = $this->apiModel->edit_product($slug, $data);
