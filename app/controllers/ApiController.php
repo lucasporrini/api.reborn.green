98b215257f1d14e8c78e4c634aec9c7d2c6cfe69
lucasporrini <2608lucas@gmail.com>
2024-03-01 15:41:14 +0100

    Feature: add product from PowerApp

--- app/controllers/ApiController.php
+++ app/controllers/ApiController.php
@@ -495,7 +495,7 @@ class ApiController
         $token = $headers['Authorization'];
         
         if($this->apiModel->middleware_auth($token)) {
-            $temp = `[
+            $temp = '[
                 {
                     "@odata.etag": "\"4\"",
                     "ItemInternalId": "8",
@@ -656,8 +656,7 @@ class ApiController
                     "{HasAttachments}": true,
                     "{VersionNumber}": "2.0"
                 }
-            ]
-            `;
+            ]';
             
             $data = json_decode($temp, true);
         
