a7d41c9884177c91fc80c91190931dd404dee760
lucasporrini <2608lucas@gmail.com>
2024-04-15 11:38:09 +0200

    Debug: add_product_from_app api route

--- app/controllers/ApiController.php
+++ app/controllers/ApiController.php
@@ -434,6 +434,7 @@ class ApiController
         // On récupère le token dans le header
         $headers = apache_request_headers();
         $token = $headers['Authorization'];
+        dd($token);
         
         if($this->apiModel->middleware_auth($token)) {
             // Récupérer les données
